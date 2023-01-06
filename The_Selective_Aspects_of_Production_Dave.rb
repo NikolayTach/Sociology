9c4aa388f15c800214061dd846bff5ad95e1ee7c
NikolayTach <nikolai.massine@gmail.com>
2023-01-06 10:13:16 -0800

    Create ruby_log.rb

---
+++ The_Selective_Aspects_of_Production_Dave.rb
@@ -0,0 +1,6 @@
+book_to_author = {
+"The Selective Aspects of Production Development from the Point of View of Modern Sociogenetics" => "Sergienko",
+"Sociology" => "Lyubov",
+"9780578914541" => "Lyubov"
+}
+
